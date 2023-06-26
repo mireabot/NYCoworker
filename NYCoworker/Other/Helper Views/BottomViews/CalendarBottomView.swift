a0adadd6c2ef1bc67607adf74e86fede4d76457c
Michael Kolkov <55915846+mireabot@users.noreply.github.com>
2023-06-26 17:18:17 -0400

    Fixed calendar in review section

--- NYCoworker/Other/Helper Views/BottomViews/CalendarBottomView.swift
+++ NYCoworker/Other/Helper Views/BottomViews/CalendarBottomView.swift
@@ -33,68 +33,73 @@ struct CalendarBottomView: View {
   }
   
   private func generateDatesForCurrentMonth() -> [Date] {
+    let currentDate = calendar.startOfDay(for: Date())
     let startOfMonth = calendar.date(from: calendar.dateComponents([.year, .month], from: selectedDate))!
     let range = calendar.range(of: .day, in: .month, for: selectedDate)!
     let days = Range(uncheckedBounds: (lower: 1, upper: range.upperBound))
-    return days.map { calendar.date(byAdding: .day, value: $0 - 1, to: startOfMonth)! }
+    
+    return days.compactMap { day in
+      let date = calendar.date(byAdding: .day, value: day - 1, to: startOfMonth)!
+      return calendar.isDate(date, inSameDayAs: currentDate) || date < currentDate ? date : nil
+    }
   }
   
   var body: some View {
     VStack(alignment: .center) {
       GrabberView()
       VStack(alignment: .leading) {
-          HStack {
-            Button {
-              showDate.toggle()
-            } label: {
-              Text("Cancel")
-                .foregroundColor(Resources.Colors.primary)
-                .font(Resources.Fonts.medium(withSize: 17))
-            }
-
-            Spacer()
-            Button {
-              date = selectedDate
-              showDate.toggle()
-            } label: {
-              Text("Done")
-                .foregroundColor(Resources.Colors.primary)
-                .font(Resources.Fonts.medium(withSize: 17))
-            }
-
+        HStack {
+          Button {
+            showDate.toggle()
+          } label: {
+            Text("Cancel")
+              .foregroundColor(Resources.Colors.primary)
+              .font(Resources.Fonts.medium(withSize: 17))
           }
-          .padding([.leading,.trailing], 10)
-          .padding(.top, 15)
-          .padding(.bottom, 10)
           
-          Text(currentMonthYear)
-            .foregroundColor(Resources.Colors.customBlack)
-            .font(Resources.Fonts.medium(withSize: 20))
-            .padding(.leading, 10)
-          VStack {
-            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 5), count: 7), spacing: 5) {
-              ForEach(0..<generateDatesForCurrentMonth().count, id: \.self) { index in
-                Button(action: {
-                  DispatchQueue.main.async {
-                    withAnimation(.spring()) {
-                      selectDate(generateDatesForCurrentMonth()[index])
-                    }
+          Spacer()
+          Button {
+            date = selectedDate
+            showDate.toggle()
+          } label: {
+            Text("Done")
+              .foregroundColor(Resources.Colors.primary)
+              .font(Resources.Fonts.medium(withSize: 17))
+          }
+          
+        }
+        .padding([.leading,.trailing], 10)
+        .padding(.top, 15)
+        .padding(.bottom, 10)
+        
+        Text(currentMonthYear)
+          .foregroundColor(Resources.Colors.customBlack)
+          .font(Resources.Fonts.medium(withSize: 20))
+          .padding(.leading, 10)
+        VStack {
+          LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 5), count: 7), spacing: 5) {
+            ForEach(0..<generateDatesForCurrentMonth().count, id: \.self) { index in
+              Button(action: {
+                DispatchQueue.main.async {
+                  withAnimation(.spring()) {
+                    selectDate(generateDatesForCurrentMonth()[index])
                   }
-                }) {
-                  Text(formatter.string(from: generateDatesForCurrentMonth()[index]))
-                    .font(isDateSelected(generateDatesForCurrentMonth()[index]) ? Resources.Fonts.medium(withSize: 15) : Resources.Fonts.regular(withSize: 15))
-                    .foregroundColor(isDateSelected(generateDatesForCurrentMonth()[index]) ? Color.white : Resources.Colors.customBlack)
-                    .padding(10)
-                    .background(isDateSelected(generateDatesForCurrentMonth()[index]) ? Resources.Colors.primary : Color.clear)
-                    .clipShape(Circle())
                 }
+              }) {
+                Text(formatter.string(from: generateDatesForCurrentMonth()[index]))
+                  .font(isDateSelected(generateDatesForCurrentMonth()[index]) ? Resources.Fonts.medium(withSize: 15) : Resources.Fonts.regular(withSize: 15))
+                  .foregroundColor(isDateSelected(generateDatesForCurrentMonth()[index]) ? Color.white : Resources.Colors.customBlack)
+                  .padding(10)
+                  .background(isDateSelected(generateDatesForCurrentMonth()[index]) ? Resources.Colors.primary : Color.clear)
+                  .clipShape(Circle())
               }
             }
           }
         }
-        .padding(.bottom, 30)
-        .background(Color.white)
-        .cornerRadius(16, corners: [.topLeft,.topRight])
+      }
+      .padding(.bottom, 30)
+      .background(Color.white)
+      .cornerRadius(16, corners: [.topLeft,.topRight])
     }
   }
 }
